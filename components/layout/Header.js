82acd3fe079fcfd956a5d298d08c1040314492e1
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-02 16:45:15 -0500

    Se creo el componente Navegacion

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Buscar from '../ui/Buscar';
+import Navegacion from './Navegacion';
+import Link from 'next/link';
 
 const Header = () => {
     return ( 
@@ -10,11 +12,17 @@ const Header = () => {
 
                     <Buscar />
 
-                    {/* Nav Aqui */}
+                    <Navegacion />
+
                 </div>
 
                 <div>
-                    {/* Menu de Administracion */}
+                    <p>Hola: Lisniel</p>
+
+                    <button type="button">Cerrar Sesión</button>
+
+                    <Link href="/">Login</Link>
+                    <Link href="/">Crear Cuenta</Link>
                 </div>
             </div>
         </header>
