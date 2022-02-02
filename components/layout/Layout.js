15adeb2aa6e872adfe7d8972b6a51566d2e65e91
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-02 16:36:45 -0500

    Se creo el componente Header y el componente Buscar

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 const Layout = props => {
     return (
         <>
 
-            <h1>Header</h1>
-            
+            <Header />
+
             <main>
                 {props.children}
             </main>
