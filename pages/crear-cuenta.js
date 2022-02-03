0d9301f3bf43e3b43d1e7aef4e3cbf80ec77917d
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-03 15:55:27 -0500

    Se añadio estilos para el Formulario, se creó el custom hook useValidacion y se creó el formulario de Crear Cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Layout from '../components/layout/layout';
+import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario';
 
 export default function CrearCuenta() {
     return (
         <div>
             <Layout>
-                <h1>Crear Cuenta</h1>
+                <>
+                    <h1
+                        css={css`
+                            text-align: center;
+                            margin-top: 5rem;
+                        `}
+                    >Crear Cuenta</h1>
+
+                    <Formulario>
+                        <Campo>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                name="nombre"
+                                placeholder="Tu Nombre"
+                            />
+                        </Campo>
+
+                        <Campo>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Tu Email"
+                            />
+                        </Campo>
+
+                        <Campo>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Tu Password"
+                            />
+                        </Campo>
+
+                        <InputSubmit
+                            type="submit"
+                            value="Crear Cuenta"
+                        />
+                    </Formulario>
+                </>
             </Layout>
         </div>
     )
