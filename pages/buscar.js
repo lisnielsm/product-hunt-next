a198b5d9414c21d4a80b755c165824a82ccc540f
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-25 19:47:35 -0500

    Se creo la funcion de buscar los productos por nombre y por descripcion

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/layout/layout';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 export default function Buscar() {
+
+    const router = useRouter();
+    const { query: { q } } = router;
+
+    // todos los productos
+    const { productos } = useProductos("creado");
+    const [resultado, guardarResultado] = useState([]);
+
+    useEffect(() => {
+        const busqueda = q.toLowerCase();
+        const filtro = productos.filter(producto => {
+            return (
+                producto.nombre.toLowerCase().includes(busqueda) ||
+                producto.descripcion.toLowerCase().includes(busqueda) 
+            )
+        })
+
+        guardarResultado(filtro);
+    }, [q, productos])
+
     return (
         <div>
             <Layout>
-                <h1>Buscar</h1>
+                <div className="listado-productos">
+                    <div className="contenedor">
+                        <ul className="bg-white">
+                            {resultado.map(producto => (
+                                <DetallesProducto
+                                    key={producto.id}
+                                    producto={producto}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </Layout>
         </div>
     )
